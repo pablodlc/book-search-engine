2aba765af3e033a4314b681897290ea18e8688b8
pablo de la cruz <pablodlc@gmail.com>
2021-08-21 00:28:22 -0700

    set up /schemas and resolvers.js, typeDefs.js, and index.js for Apollo

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(content: BookInfo!): User
+        removeBook(bookId: String!): User
+    }
+
+    input BookInfo {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
